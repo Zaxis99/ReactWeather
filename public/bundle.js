c025c54cbd7b8e35dee71fe1788b9936d7bb2d0c
Zack Harvey <zackh99@gmail.com>
2017-09-15 11:34:24 -0300

    Updated About Page

--- public/bundle.js
+++ public/bundle.js
@@ -28797,9 +28797,18 @@ var React = __webpack_require__(6);
 
 var About = function About(props) {
     return React.createElement(
-        'h3',
+        'div',
         null,
-        'About Component'
+        React.createElement(
+            'h3',
+            null,
+            'About'
+        ),
+        React.createElement(
+            'p',
+            null,
+            'Welcome to the about page.'
+        )
     );
 };
 
